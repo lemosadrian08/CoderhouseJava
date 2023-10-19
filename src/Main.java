b823ed11f313bd266ef857a61c1b74da1c6c9b45
Adrian <lemosadrian08@gmail.com>
2023-10-19 00:43:24 -0300

    dasafio 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float numero = 5.2f;
+        double parteDecimal = numero % 1;
+        double parteEntera = numero - parteDecimal;
+        System.out.println(parteDecimal);
+        System.out.println(parteEntera);
     }
 }
